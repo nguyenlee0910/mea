0c85009c5068c381af2ea83e1750388ef82b359c
Hieu Huynh Minh <hieu.huynh@seltec.io>
2023-12-03 03:17:32 +0700

    add ui for import request

--- lib/presentations/Request/choose_supply_import_request.dart
+++ lib/presentations/Request/choose_supply_import_request.dart
@@ -1,6 +1,7 @@
-import 'package:anim_search_bar/anim_search_bar.dart';
 import 'package:easy_search_bar/easy_search_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:gap/gap.dart';
 import 'package:mea/models/supply_model.dart';
 import 'package:mea/services/department_api.dart';
 
@@ -14,6 +15,7 @@ class ListSupplyPage extends StatefulWidget {
 
 class _ListSupplyPageState extends State<ListSupplyPage> {
   List<SupplyModel> listSupply = [];
+  List<SupplyModel> filterListSupply = [];
   TextEditingController textController = TextEditingController();
 
   Future<void> getListSupply() async {
@@ -33,17 +35,80 @@ class _ListSupplyPageState extends State<ListSupplyPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: EasySearchBar(
-        isFloating: true,
-        title: const Text('Example'),
-        onSearch: (value) => setState(() {}),
-        actions: [IconButton(icon: const Icon(Icons.person), onPressed: () {})],
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [],
+        appBar: EasySearchBar(
+          isFloating: true,
+          title: const Text('Example'),
+          onSearch: (value) => setState(() {
+            ;
+          }),
         ),
-      ),
+        body: GridView.count(
+          primary: false,
+          padding: const EdgeInsets.all(20),
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+          crossAxisCount: 2,
+          children: listSupply.map(
+            (e) {
+              return buildCell(context, e);
+            },
+          ).toList(),
+        ));
+  }
+
+  Widget buildCell(BuildContext context, SupplyModel supplyModel) {
+    Size size = MediaQuery.of(context).size;
+    return Column(
+      children: [
+        Expanded(
+          child: SizedBox.square(
+            dimension: size.width * 0.35,
+            child: Stack(
+              children: [
+                Neumorphic(
+                  style: NeumorphicStyle(
+                    boxShape:
+                        NeumorphicBoxShape.roundRect(BorderRadius.circular(20)),
+                    depth: 5,
+                    color: Colors.grey,
+                    intensity: 1,
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.network(
+                      supplyModel.imageUrls!.first,
+                      fit: BoxFit.fill,
+                      loadingBuilder: (context, child, loadingProgress) {
+                        if (loadingProgress == null) {
+                          return child;
+                        } else {
+                          return const CircularProgressIndicator();
+                        }
+                      },
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.add_circle,
+                    color: Colors.green,
+                  ),
+                  onPressed: () {},
+                )
+              ],
+            ),
+          ),
+        ),
+        const Gap(16),
+        Align(
+          alignment: Alignment.topLeft,
+          child: Text(
+            supplyModel.name!,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+          ),
+        )
+      ],
     );
   }
 }
